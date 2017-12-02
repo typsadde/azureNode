58c9ae9005590913f972af6c59b2fee907b47305
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 19:34:13 +0100

    shit works

--- routes.js
+++ routes.js
@@ -1,11 +1,15 @@
+var request = require('request');
 var appRouter = function(app) {
-    
+
     app.get("/", function(req, res) {
         res.send("Hello World");
     });
 
     app.get("/hello", function(req, res) {
-        res.send("Hello from hello path");
+        request("https://api.fixer.io/latest", function(error,response,body){
+            res.send(body);
+        });
+        //res.send("Hello from hello path");
     });
 }
     
