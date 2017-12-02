48a47ab5c940edc00996e3e2efc852cdd89f54ad
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 19:14:38 +0100

    shit works

--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 var appRouter = function(app) {
+    
     app.get("/", function(req, res) {
         res.send("Hello World");
+    });
+
+    app.get("/hello", function(req, res) {
+        res.send("Hello from hello path");
     });
 }
     
