254297f060049dbecbba6733859a0e1415151991
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 01:32:32 +0100

    currency trying

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var http = require('http');
 //var request = require('request');
 
-const https = require("https");
-const url =
-  "https://maps.googleapis.com/maps/api/geocode/json?address=Florence";
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  res.on("end", () => {
-    body = JSON.parse(body);
-    console.log(
-      `City: ${body.results[0].formatted_address} -`,
-      `Latitude: ${body.results[0].geometry.location.lat} -`,
-      `Longitude: ${body.results[0].geometry.location.lng}`
-    );
-  });
-});
-
 var server = http.createServer(function(request, response) {
-    var temp = "This is text"
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.end(temp);
+    
+    const https = require("https");
+    const url =
+      "https://maps.googleapis.com/maps/api/geocode/json?address=Florence";
+    https.get(url, res => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        response.writeHead(200, {"Content-Type": "application/json"});        
+        response.end(
+          `City: ${body.results[0].formatted_address} -`,
+          `Latitude: ${body.results[0].geometry.location.lat} -`,
+          `Longitude: ${body.results[0].geometry.location.lng}`
+        );
+      });
+    });
+    
+    //var temp = "This is text"
+   // response.end(temp);
 });
 
 var port = process.env.PORT || 1337;
