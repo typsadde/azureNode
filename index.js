4f9dcdce64bb6a26c89bea2a2b5b848c274b8fd9
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 01:33:21 +0100

    currency trying

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var server = http.createServer(function(request, response) {
       });
       res.on("end", () => {
         body = JSON.parse(body);
-        response.writeHead(200, {"Content-Type": "application/json"});        
         response.end(
           `City: ${body.results[0].formatted_address} -`,
           `Latitude: ${body.results[0].geometry.location.lat} -`,
@@ -23,8 +22,9 @@ var server = http.createServer(function(request, response) {
       });
     });
     
-    //var temp = "This is text"
-   // response.end(temp);
+    var temp = "This is text"
+    response.writeHead(200, {"Content-Type": "application/json"});
+    //response.end(temp);
 });
 
 var port = process.env.PORT || 1337;
