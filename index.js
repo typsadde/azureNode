24f47c45e0c0a730b9add17872853194e9e38815
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 02:09:59 +0100

    currency trying

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ var server = http.createServer(function(request, response) {
     });
     res.on("end", () => {
       body = JSON.parse(body);
-      body = this.body;
+      response.end(body);
     });
   });
 
     var temp = "This is text"
     response.writeHead(200, {"Content-Type": "application/json"});
-   response.end(body);
+    //response.end(body);
 });
 
 var port = process.env.PORT || 1337;
