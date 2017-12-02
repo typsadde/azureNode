b90d35a9a5c0542b840f902b3e44965a36c76e59
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-02 10:26:35 +0100

    shit works

--- index.js
+++ index.js
@@ -1,58 +1,36 @@
-// Authors:
-// Shane Oatman https://github.com/shoatman
-// Sunil Bandla https://github.com/sunilbandla
-// Daniel Dobalian https://github.com/danieldobalian
+var http = require('http');
+const https = require("https");
+const express = require("express");
 
-var express = require("express");
-var morgan = require("morgan");
-var passport = require("passport");
-var BearerStrategy = require('passport-azure-ad').BearerStrategy;
+var router = express.Router();
 
-var options = {
-    identityMetadata: "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration/",
-    clientID: "85327f73-fd44-46b9-a159-28544ff72288",
-    validateIssuer: false,
-    loggingLevel: 'warn',
-    passReqToCallback: false
-};
-
-// Check for client id placeholder
-if (options.clientID === 'YOUR_CLIENT_ID') {
-    console.error("Please update 'options' with the client id (application id) of your application");
-    return;
-}
-
-var bearerStrategy = new BearerStrategy(options,
-    function (token, done) {
-        // Send user info using the second argument
-        done(null, {}, token);
-    }
-);
-
-var app = express();
-app.use(morgan('dev'));
-
-app.use(passport.initialize());
-passport.use(bearerStrategy);
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
-    next();
+router.get('/hello', function(req,res){
+    res.json({message:'This is a get request'})
 });
 
-app.get("/hello",
-    passport.authenticate('oauth-bearer', {session: false}),
-    function (req, res) {
-        var claims = req.authInfo;
-        console.log('User info: ', req.user);
-        console.log('Validated claims: ', claims);
-        
-        res.status(200).json({'name': claims['name']});
-    }
-);
+var server = http.createServer(function(request, response) {
+    const url =
+    "https://api.fixer.io/latest";
 
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log("Listening on port " + port);
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => {
+      //body = JSON.parse(body);
+      response.end(body);
+    });
+  });
+
+    var temp = "This is text"
+    response.writeHead(200, {"Content-Type": "application/json"});
 });
+
+var port = process.env.PORT || 1337;
+server.listen(port); 
+
+console.log("Server running at http://localhost:%d", port);
+
+
