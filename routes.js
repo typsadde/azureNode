f806c8ebf982d6e19d96e8593b4d4fe97912e383
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-12-04 15:57:54 +0100

    json stringify

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ var appRouter = function(app) {
             var script = $('script').first().toString();
             script = script.replace('<script>','')
             script = script.replace('</script>','')
-            //script = script.replace(/\s/g,'')
+            script = script.replace(/\s/g,'')
             script = script.replace('var weekData = ','')
             script = script.replace(/new Date/g,"")
             //script = script.replace(/"[(]"/g,"")
